cde029e61863dfc026c4713af70729d00a989e26
Raissa <raissa.demczuk@gmail.com>
2026-05-10 16:37:26 -0300

    feat:criando os cards

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const listaProdutos = [
+ {nome: "HOMEM DE FERRO", descricao: "Boneco ilustrativo do superheroi Homem de ferro",preco: 4500,imagem: ""},
+ {nome: "SUPERMAN",descricao: "Boneco ilustrativo do superheroi Superman",preco: 2200,imagem: ""},
+ {nome: "BATMAN",descricao: "Boneco ilustrativo do superheroi Batman",preco: 350,imagem: ""},
+ {nome: "FLASH",descricao: "Boneco ilustrativo do superheroi Flash",preco: 180,imagem: ""},
+ {nome: "HULK",descricao: "Boneco ilustrativo do superheroi Hulk",preco: 420,imagem: ""}
+];
