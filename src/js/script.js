69c3c5a02c1eb66b0609439f9502f3be39a8f370
Raissa <raissa.demczuk@gmail.com>
2026-05-10 16:41:54 -0300

    feat:exibindo os cards

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,14 @@ const listaProdutos = [
  {nome: "FLASH",descricao: "Boneco ilustrativo do superheroi Flash",preco: 180,imagem: ""},
  {nome: "HULK",descricao: "Boneco ilustrativo do superheroi Hulk",preco: 420,imagem: ""}
 ];
+
+const container = document.getElementById("produtos");
+listaProdutos.forEach(produto => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}">
+    <h3>${produto.nome}</h3>
+    <p>${produto.descricao}</p>
+    <p class="preco">R$ ${produto.preco}</p>`;
+container.appendChild(card);
+});
