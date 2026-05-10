b43c8b6a2cc38ae451e2506c37e8662822404ddc
MurilloFonseca <murillofonseca127@gmail.com>
2026-05-10 17:57:50 -0300

    style: formatados os arquivos html, css e js

--- src/js/loja.js
+++ src/js/loja.js
@@ -1,47 +1,48 @@
 // ARRAY DO CARRINHO
-    const carrinho = [
- {nome: "HOMEM DE FERRO", preco: 250,imagem: ""},
- {nome: "SUPERMAN",preco: 200,imagem: ""},
- {nome: "BATMAN",preco: 300,imagem: ""},
- {nome: "FLASH",preco: 150,imagem: ""},
- {nome: "HULK",preco: 200,imagem: ""}
+const carrinho = [
+  { nome: "HOMEM DE FERRO", preco: 250, imagem: "" },
+  { nome: "SUPERMAN", preco: 200, imagem: "" },
+  { nome: "BATMAN", preco: 300, imagem: "" },
+  { nome: "FLASH", preco: 150, imagem: "" },
+  { nome: "HULK", preco: 200, imagem: "" },
 ];
 
 const listaItens = document.getElementById("listaItens");
 const valorTotal = document.getElementById("valorTotal");
 const btnDesconto = document.getElementById("btnDesconto");
 
-    // MOSTRAR ITENS NA TELA USANDO DOM
-carrinho.forEach(item => {
+// MOSTRAR ITENS NA TELA USANDO DOM
+carrinho.forEach((item) => {
+  const div = document.createElement("div");
+  div.classList.add("item");
 
-    const div = document.createElement("div");
-    div.classList.add("item");
-
-    div.innerHTML = `
+  div.innerHTML = `
         <span>${item.nome}</span>
         <span>
           ${item.preco.toLocaleString("pt-BR", {
-            style: "currency", currency: "BRL"
+            style: "currency",
+            currency: "BRL",
           })}
         </span>
     `;
-    listaItens.appendChild(div);
+  listaItens.appendChild(div);
 });
 
 let total = carrinho.reduce((soma, item) => {
-    return soma + item.preco;
+  return soma + item.preco;
 }, 0);
 
-function formatarMoeda(valor){
-    return valor.toLocaleString("pt-BR", {
-      style: "currency", currency: "BRL"
-    });
+function formatarMoeda(valor) {
+  return valor.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
 valorTotal.textContent = formatarMoeda(total);
 btnDesconto.addEventListener("click", () => {
-    total = total - (total * 0.10);
-    valorTotal.textContent = formatarMoeda(total);
-    btnDesconto.disabled = true;
-    btnDesconto.textContent = "Desconto Aplicado";
-    btnDesconto.style.background = "green";
+  total = total - total * 0.1;
+  valorTotal.textContent = formatarMoeda(total);
+  btnDesconto.disabled = true;
+  btnDesconto.textContent = "Desconto Aplicado";
+  btnDesconto.style.background = "green";
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,43 @@
 const listaProdutos = [
- {nome: "HOMEM DE FERRO", descricao: "Boneco ilustrativo do superheroi Homem de ferro",preco: 250,imagem: ""},
- {nome: "SUPERMAN",descricao: "Boneco ilustrativo do superheroi Superman",preco: 200,imagem: ""},
- {nome: "BATMAN",descricao: "Boneco ilustrativo do superheroi Batman",preco: 300,imagem: ""},
- {nome: "FLASH",descricao: "Boneco ilustrativo do superheroi Flash",preco: 150,imagem: ""},
- {nome: "HULK",descricao: "Boneco ilustrativo do superheroi Hulk",preco: 200,imagem: ""}
+  {
+    nome: "HOMEM DE FERRO",
+    descricao: "Boneco ilustrativo do superheroi Homem de ferro",
+    preco: 250,
+    imagem: "",
+  },
+  {
+    nome: "SUPERMAN",
+    descricao: "Boneco ilustrativo do superheroi Superman",
+    preco: 200,
+    imagem: "",
+  },
+  {
+    nome: "BATMAN",
+    descricao: "Boneco ilustrativo do superheroi Batman",
+    preco: 300,
+    imagem: "",
+  },
+  {
+    nome: "FLASH",
+    descricao: "Boneco ilustrativo do superheroi Flash",
+    preco: 150,
+    imagem: "",
+  },
+  {
+    nome: "HULK",
+    descricao: "Boneco ilustrativo do superheroi Hulk",
+    preco: 200,
+    imagem: "",
+  },
 ];
 
 const container = document.getElementById("produtos");
-listaProdutos.forEach(produto => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}">
+listaProdutos.forEach((produto) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}">
     <h3>${produto.nome}</h3>
     <p>${produto.descricao}</p>
     <p class="preco">R$ ${produto.preco}</p>`;
-container.appendChild(card);
+  container.appendChild(card);
 });
