9ae6647c7209073f533b3dce7785d2c5f059a568
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-11 09:10:40 -0300

    feat: fazendo os últimos ajustes do projeto

--- src/js/loja.js
+++ src/js/loja.js
@@ -1,22 +1,17 @@
-// ARRAY DO CARRINHO
-    const carrinho = [
+const carrinho = [
  {nome: "HOMEM DE FERRO", preco: 250,imagem: ""},
  {nome: "SUPERMAN",preco: 200,imagem: ""},
  {nome: "BATMAN",preco: 300,imagem: ""},
  {nome: "FLASH",preco: 150,imagem: ""},
  {nome: "HULK",preco: 200,imagem: ""}
 ];
-
 const listaItens = document.getElementById("listaItens");
 const valorTotal = document.getElementById("valorTotal");
 const btnDesconto = document.getElementById("btnDesconto");
 
-    // MOSTRAR ITENS NA TELA USANDO DOM
 carrinho.forEach(item => {
-
     const div = document.createElement("div");
     div.classList.add("item");
-
     div.innerHTML = `
         <span>${item.nome}</span>
         <span>
@@ -27,7 +22,6 @@ carrinho.forEach(item => {
     `;
     listaItens.appendChild(div);
 });
-
 let total = carrinho.reduce((soma, item) => {
     return soma + item.preco;
 }, 0);
